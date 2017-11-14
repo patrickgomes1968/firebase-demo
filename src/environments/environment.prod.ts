e24b621179c34ec2e94c025152d16a35f9b02135
Patrick Gomes <patrickgomes@gmail.com>
2017-11-14 12:04:57 -0600

    updated env.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCAwmXLFvPwI0sw9u3HHyDf_mVQf5v5k2I',
+    authDomain: 'fir-demo-b144a.firebaseapp.com',
+    databaseURL: 'https://fir-demo-b144a.firebaseio.com',
+    projectId: 'fir-demo-b144a',
+    storageBucket: 'fir-demo-b144a.appspot.com',
+    messagingSenderId: '371001556362'
+  }
 };
